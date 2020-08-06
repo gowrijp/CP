3b6ad6e026b933a296c346d1e725613befa8a52c
Gowri Jayaprakash <34152914+gowrijp@users.noreply.github.com>
2020-08-06 19:57:22 +0530

    Create longest-valid-parenthesis.cpp

---
+++ stack/longest-valid-parenthesis.cpp
@@ -0,0 +1,32 @@
+/*
+https://www.interviewbit.com/problems/longest-valid-parentheses/
+
+SOLUTION using stacks 
+
+*/
+int Solution::longestValidParentheses(string A) {
+    int n = A.length();
+    if(n<=1)
+        return 0;
+    stack<int> indices;
+    indices.push(-1);
+    int last = -1;
+    int res = 0;
+    for(int i = 0; i<n; i++){
+        if(A[i]=='(')
+            indices.push(i);
+        else{
+            int y = indices.top();
+            if(y==last){
+                last = i;
+                indices.push(i);
+            }
+            else{
+                indices.pop();
+                y = indices.top();
+                res = max(res, i-y);
+            }
+        }
+    }
+    return res;
+}
