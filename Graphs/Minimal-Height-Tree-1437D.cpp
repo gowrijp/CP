7dfd98ed1cb8d419d6f0ce82133fddb505151bad
Gowri Jayaprakash <34152914+gowrijp@users.noreply.github.com>
2020-10-29 02:41:08 +0530

    BFS- finding height of tree **

--- Graphs/Minimal-Height-Tree-1437D.cpp
+++ Graphs/Minimal-Height-Tree-1437D.cpp
@@ -2,27 +2,27 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        int arr[n];
-        for(int i=0;i<n;i++){
-            cin>>arr[i];
-        }
-        vector<int>h(n,int(1e9));
-        h[0]=0;
  
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++) cin>>a[i];
+        vector<int> h(n,0);
+        h[0]=0;
         int cnt=0;
         for(int i=1;i<n;i++){
-            if(arr[i]<arr[i-1]){
+            if(a[i]<a[i-1]){
                 cnt++;
             }
             h[i]=h[cnt]+1;
         }
-        cout<<h[n-1]<<endl;
+        cout<<h[n-1]<<"\n";
     }
     return 0;
 }
